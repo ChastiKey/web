74185ac7d159c56ac6726668bf957e3bdc53b527
Emma <emma@tdm.io>
2019-11-13 21:01:52 -0400

    Updating Deploy & vue.config for new repo location

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:rileyio/chastikey-web.git master:gh-pages
+git push -f git@github.com:ChastiKey/web.git master:gh-pages
 
 cd -
